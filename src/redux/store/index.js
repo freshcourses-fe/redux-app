66f6c99b1049a179a180fcaac650b139604c9b70
Kirill <freshjs.ftd.@gmail.com>
2022-05-27 20:56:20 +0300

    Refactor store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { legacy_createStore as  createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import reducer from '../reducers';
